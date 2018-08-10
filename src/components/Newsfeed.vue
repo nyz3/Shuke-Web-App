aab362fceced8265bb0eb84c69c0ea4f88333db1
Nathan Zheng <nyz3@cornell.edu>
2018-08-10 22:23:01 +0800

    final push

--- src/components/Newsfeed.vue
+++ src/components/Newsfeed.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="feedcontainer">
-        <span v-html="content"></span> <!--content injected here during page load-->
+    
     </div>
 </template>
 
@@ -11,28 +11,64 @@
 
 import axios from 'axios' /*axios and vueAxios are wrapper classes for automating API calls below in the "ajax" function*/
 import VueAxios from 'vue-axios'
+import newsAndButtons from './subcomponents/NewsAndButtons.vue'
 
 export default {
     name: 'home',
     data () {
         return {
-            msg: 'Welcome to Your Vue.js App',
-            content: ''
+            posts: [
+                
+            ]
         }
     },
     methods: {
         
     },
+    components: {
+        newsAndButtons    
+    },
     created() {                   /*automatically called when router routes to this page, should retrieve content from API*/
       
-    axios.get('/api/article/follow/{userid}').then((response) => { /*REPLACE userid with ACTUAL USERID*/
 
-        for(var i = 0; i < response.data.data.list.length; i++)
-        {
-            var post = response.data.data.list[i].content;
-            this.content += ('<div class="postcontainer">' + post + '</div>');
-        }
+    axios.get('/api/article/follow/{userid}').then((response) => { /*articles have article id, answers have answerid, questions have */
+        
+        //DO SOMETHING SIMILAR TO THIS, THIS IS NOT EXACT
 
+
+        // for(var i = 0; i < response.data.data.list.length; i++)
+        // {
+        //     var info = responses.data.data.list[i]; //contains all info about a single post, from the API callback.
+
+        //     //ID is necessary for identification of post type
+        //     var answerID = info.answerid;
+        //     var articleID = info.articleid;
+        //     var questionID = info.questionid;
+
+        //     var postType;
+        //     var uniqueID;
+
+        //     if(answerID != null){
+        //         postType = 'answer';
+        //         uniqueID = answerID;
+        //     } else if(articleID != null){
+        //         postType = 'article';
+        //         uniqueID = articleID;
+        //     } else {
+        //         postType = 'question';
+        //         uniqueID = questionID;
+        //     }
+
+        //     var post = info.content; /*text content in each post*/
+        //     var tokens = info.tokens; //NEEDS UPDATING
+        //     var likes = info.praisecount;
+        //     var dislikes = info.dislikes;
+        //     var comments = info.comments;
+
+        //     var postData = {postType, uniqueID, post, tokens, likes, dislikes, comments};
+        //     posts.add(postData);    //add each set of post data as an object into the post array in 'data'
+
+        // }
     });
   }
 }
--- src/components/SearchContent.vue
+++ src/components/SearchContent.vue
@@ -15,6 +15,7 @@ export default {
     data () {
         return {
            searchParams: null,
+           searchError: null,           //searchError default null, if results present, becomes true, else false
            articlesFound: [
 
                //each object in this array should represent an article and its associated data.
@@ -34,11 +35,14 @@ export default {
         axios.get('/api/article/search/?articletitle=' + this.searchParams +'&pagenum=1&pagesize=10').then((response) => { /*articles have article id, answers have answerid, questions have */
             if(response.data.data.list.length = 0) {  //no matching results
                 //let user know that no results are available
+                //use v-if directive in html, 
+                searchError = true;
             } else {
                 for(var i = 0; i < response.data.data.list.length; i++) {
             
-                    //take retrieved data and process
-                    
+                    //take retrieved data and process, and add each post/info into articlesFound as an object
+                    articlesFound.add(null);
+                    searchError = false; //when switched to false, search results will be displayed
                 }
             }
         });
